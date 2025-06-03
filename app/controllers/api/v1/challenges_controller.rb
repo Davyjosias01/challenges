7ece77a187e2c7441fa1eb9bc4a4dbcc86022317
davy <davy.josias@gmail.com>
2025-06-03 17:32:34 -0300

    ajuste no ChallengeController

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ChallengesController < ApplicationController
+  before_action :set_challenge, only: %i[ show update destroy ]
+
   def index
     # Show all the challenges
     @challenges = Challenge.all
@@ -7,17 +9,46 @@ class Api::V1::ChallengesController < ApplicationController
 
   def create
     # Create a single challenge
+    challenge = Challenge.new(challenge_params)
+
+    if challenge.save
+      render json: { message: "Challenge added successfully", data: challenge }
+    else
+      render json: { message: "Failed to add challenge", data: challenge.errors }
+    end
   end
 
   def show
     # Show a single challenge
+    # challenge = Challenge.find(params[:id])
+    if @challenge
+      render json: { message: "Challenge found!", data: @challenge }
+    else
+      render json: { message: "Challenge not found!", data: @challenge.errors }
+    end
   end
 
   def update
     # Update a single challenge
+    # challenge = Challenge.find(params[:id])
+    if @challenge.update(challenge_params)
+      render json: { message: "Challenge updated", data: @challenge }
+    else
+      render json: { message: "Challenge not updated!", data: @challenge.errors }
+    end
   end
 
   def destroy
     # Delete single challenge
+    if @challenge.destroy
+  end
+
+  private
+  def set_challenge
+    @challenge = Challenge.find(params[:id])
+  end
+
+  def challenge_params
+    params.required(:challenge).permit(:title, :description, :start_date, :end_date)
   end
 end
